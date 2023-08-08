bdb324bc514607168ff3b64a38de98584dd78188
sakshisharma1007 <Sakshi.A.Sharma@geminisolutions.com>
2023-08-08 20:13:53 +0530

    understanding window

--- file2.js
+++ file2.js
@@ -13,3 +13,14 @@ function b(){
     var example = 100;
     console.log(example);
 }
+//understanding window:-
+
+var x=10;
+function c(){
+    var test= "String";
+}
+
+console.log(window.x);   //or
+console.log(x);   //or
+console.log(this.x);
+
